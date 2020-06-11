7ee741179901337eebc96933a0e1d2118c0bbacf
Brynner Ferreira <ferreira@brynner.net>
2020-06-11 20:54:03 -0300

    Gap Component

---
+++ src/components/ui/Gap.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledGap = styled.h1`
+  margin-bottom: 25px;
+`;
+
+function Gap() {
+  return (
+    <StyledGap />
+  );
+
+}
+
+export default Gap;
